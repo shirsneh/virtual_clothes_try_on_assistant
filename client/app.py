c693401508ba018c82574d76a6cbc74fb5bcf1e7
Shir <Shirsneh54@gmail.com>
2023-09-04 14:54:08 +0300

    put rm inputs as a note

--- client/app.py
+++ client/app.py
@@ -18,7 +18,7 @@ def submit():
     model = request.files['model']
 
     ## replace the url from the ngrok url provided on the notebook on server.
-    url = "http://028c-34-121-69-6.ngrok-free.app/api/transform"
+    url = " http://17e8-34-143-197-87.ngrok-free.app/api/transform"
     print("sending")
     response = requests.post(url=url, files={"cloth": cloth.stream, "model": model.stream})
     op = Image.open(BytesIO(response.content))
